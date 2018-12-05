5f32a9a3ea15bad3b1776201980675705cfe95f6
yxq199348 <yxq199348@gmail.com>
2018-12-05 22:09:00 +0800

    前端react+界面修改

---
+++ web/src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+    <h1>React Okay</h1>
+);
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);

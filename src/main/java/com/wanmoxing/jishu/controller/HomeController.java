d16d47966fcbb3a08cd1d0240a20cd4c1384cbf7
yxq199348 <yxq199348@gmail.com>
2019-01-20 21:53:13 +0800

    页面刷新

--- src/main/java/com/wanmoxing/jishu/controller/HomeController.java
+++ src/main/java/com/wanmoxing/jishu/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 	
-	@RequestMapping("/")
+	@RequestMapping(value={"/", "/course", "/course/**"})
 	public String home() {
 		return "index.html";
 	}

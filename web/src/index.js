f09d13328f9d3a20a59005b7a6d18e55e8808547
yxq199348 <yxq199348@gmail.com>
2019-04-23 23:55:44 +0800

    eslint

--- web/src/index.js
+++ web/src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from 'react-router';
-import { Provider } from 'react-redux';
+// import { Router } from 'react-router';
+// import { Provider } from 'react-redux';
 // import router from './router';
 // import configureStore from './store';
-import { hot } from 'react-hot-loader/root';
+// import { hot } from 'react-hot-loader/root';
 
-if (module.hot) {
-    //当模块更新的时候，通知index.js
-    module.hot.accept();
-}
+// if (module.hot) {
+    // 当模块更新的时候，通知index.js
+    // module.hot.accept();
+// }
 
 // const initialState = window.__INITIAL_STATE__;
 // const store = configureStore(initialState);
-ReactDOM.render(
-    <div>Hello React!</div>,
-    document.getElementById('app')
-);
+ReactDOM.render(<div>Hello React!</div>, document.getElementById('app'));

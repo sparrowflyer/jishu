9172d7db1cbeb93153f11ef9bc133c4f2cfcd109
yxq199348 <yxq199348@gmail.com>
2019-04-27 20:00:55 +0800

    UncleJee Web Version2

---
+++ web/component/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class Navigation extends React.Component {
+    render() {
+        return (
+            <div>
+                I am Navigation
+            </div>
+        );
+    }
+}
---
+++ web/page/College.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Navigation } from '../component/Navigation.js';
+
+export class College extends React.Component {
+    render() {
+        return (
+            <div>
+                <Navigation />
+                <div>I am College</div>
+            </div>
+        );
+    }
+}

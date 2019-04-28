09ebe0080f9616ae490d92dccc540bb920cbf1f4
yxq199348 <yxq199348@gmail.com>
2019-04-28 14:03:32 +0800

    登陆注册页面初步

---
+++ web/page/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class Login extends React.Component {
+    render() {
+        return (
+            <div className="login">
+                <div className="login_item">
+
+                </div>
+            </div>
+        );
+    }
+}

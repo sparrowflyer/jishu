c93e16e34db600991808eab6c5184e239dd00825
jxf1103 <360323020@qq.com>
2019-05-15 23:55:25 +0800

    轮播功能

--- web/page/Home.jsx
+++ web/page/Home.jsx
@@ -3,21 +3,49 @@ import {Footer} from '../component/common/Footer.jsx';
 import {Header} from '../component/common/Header.jsx';
 import {Carousel} from '../component/Carousel.jsx';
 import '../assets/style.css';
-// import img from "./../assets/images/001.jpg";
 import { Link } from 'react-router-dom';
+
+const students = [{
+    name:"这是个假数据",
+    university: "伯恩茅斯大学",
+    img:"http://cdn.unclejee.cn/20190118215849_614.jpg",
+    desc:"我们可以随时随地分享我们的生活，就算在海外，因为有了这个平台，我们也能感受到生活的充实。",
+},{
+    name:"这是个假数据",
+    university: "伯恩茅斯大学",
+    img:"http://cdn.unclejee.cn/20190118215849_614.jpg",
+    desc:"我们可以随时随地分享我们的生活，就算在海外，因为有了这个平台，我们也能感受到生活的充实。",
+},{
+    name:"这是个假数据",
+    university: "伯恩茅斯大学",
+    img:"http://cdn.unclejee.cn/20190118215849_614.jpg",
+    desc:"我们可以随时随地分享我们的生活，就算在海外，因为有了这个平台，我们也能感受到生活的充实。",
+},{
+    name:"这是个假数据",
+    university: "伯恩茅斯大学",
+    img:"http://cdn.unclejee.cn/20190118215849_614.jpg",
+    desc:"我们可以随时随地分享我们的生活，就算在海外，因为有了这个平台，我们也能感受到生活的充实。",
+},{
+    name:"这是个假数据",
+    university: "伯恩茅斯大学",
+    img:"http://cdn.unclejee.cn/20190118215849_614.jpg",
+    desc:"我们可以随时随地分享我们的生活，就算在海外，因为有了这个平台，我们也能感受到生活的充实。",
+}];
 
 export class HomePage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            studentList:[]
+            width: document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth,
+            studentList: students
         };
     }
-    componentDidMount() {
-        this.setState({ studentList: [] })
-    }
+    componentDidMount() {}
 
     render() {
+        const {studentList,width} = this.state;
+       let idx = width > 768 ? 7 : 5 ;
+       let studentArr = studentList.slice(0,idx);
         return (
             <div className='home-contain'>
                 <Header></Header>
@@ -31,79 +59,18 @@ export class HomePage extends React.Component {
                     </div>
                     <div className="cards-wrap">
                         <ul className="card-list">
-                            <li>
-                                <div className="person-pic">
-                                    <img src={require("./../assets/images/001.jpg")} alt=""/>
-                                    <div className="stu-desc">我们可以随时随地分享我们的生活，就算在海外，因为有了这个平台，我们也能感受到生活的充实。</div>
-                                </div>
-                                <div className="person-name">
-                                    {/*谭敏仪*/}
-                                    <Link to="/StudentDetail">谭敏仪</Link>
-                                    <div className="person-uni"><Link to="/collegeDetail">伯恩茅斯大学</Link></div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="person-pic">
-                                    <img src={require("./../assets/images/001.jpg")} alt=""/>
-                                    <div className="stu-desc">我们可以随时随地分享我们的生活，就算在海外，因为有了这个平台，我们也能感受到生活的充实。</div>
-                                </div>
-                                <div className="person-name">
-                                    <Link to="/StudentDetail">谭敏仪</Link>
-                                    <div className="person-uni"><Link to="/collegeDetail">伯恩茅斯大学</Link></div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="person-pic">
-                                    <img src={require("./../assets/images/001.jpg")} alt=""/>
-                                    <div className="stu-desc">我们可以随时随地分享我们的生活，就算在海外，因为有了这个平台，我们也能感受到生活的充实。</div>
-                                </div>
-                                <div className="person-name">
-                                    <Link to="/StudentDetail">谭敏仪</Link>
-                                    <div className="person-uni"><Link to="/collegeDetail">伯恩茅斯大学</Link></div>
-                                </div>
-                            </li>
-                            <li className="active">
-                                <div className="person-pic">
-                                    <img src={require("./../assets/images/001.jpg")} alt=""/>
-                                    <div className="stu-desc">我们可以随时随地分享我们的生活，就算在海外，因为有了这个平台，我们也能感受到生活的充实。</div>
-                                </div>
-                                <div className="person-name">
-                                    <Link to="/StudentDetail">谭敏仪</Link>
-                                    <div className="person-uni"><Link to="/collegeDetail">伯恩茅斯大学</Link></div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="person-pic">
-                                    <img src={require("./../assets/images/001.jpg")} alt=""/>
-                                    <div className="stu-desc">我们可以随时随地分享我们的生活，就算在海外，因为有了这个平台，我们也能感受到生活的充实。</div>
-                                </div>
-                                <div className="person-name">
-                                    <Link to="/StudentDetail">谭敏仪</Link>
-                                    <div className="person-uni"><Link to="/collegeDetail">伯恩茅斯大学</Link></div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="person-pic">
-                                    <img src={require("./../assets/images/001.jpg")} alt=""/>
-                                    <div className="stu-desc">我们可以随时随地分享我们的生活，就算在海外，因为有了这个平台，我们也能感受到生活的充实。</div>
-                                </div>
-                                <div className="person-name">
-                                    <Link to="/StudentDetail">谭敏仪</Link>
-                                    <div className="person-uni"><Link to="/collegeDetail">伯恩茅斯大学</Link></div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="person-pic">
-                                    <img src={require("./../assets/images/001.jpg")} alt=""/>
-                                    <div className="stu-desc">我们可以随时随地分享我们的生活，就算在海外，因为有了这个平台，我们也能感受到生活的充实。</div>
-                                </div>
-                                <div className="person-name">
-                                    <Link to="/StudentDetail">谭敏仪</Link>
-                                    <div className="person-uni">
-                                        <Link to="/collegeDetail"><Link to="/collegeDetail">伯恩茅斯大学</Link></Link>
+                            {studentArr.map(stu => {
+                                return <li>
+                                    <div className="person-pic">
+                                        <img src={stu.img} alt=""/>
+                                        <div className="stu-desc">{stu.desc}</div>
                                     </div>
-                                </div>
-                            </li>
+                                    <div className="person-name">
+                                        <Link to="/StudentDetail">{stu.name}</Link>
+                                        <div className="person-uni"><Link to="/collegeDetail">{stu.university}</Link></div>
+                                    </div>
+                                 </li>
+                            })}
                         </ul>
                     </div>
                     <div className="ta-center">

1d00bd3c56faba1362cad384cf15a1615f2a6da1
yxq199348 <yxq199348@gmail.com>
2019-05-22 23:33:22 +0800

    二级路径

--- src/main/java/com/wanmoxing/jishu/controller/HomeController.java
+++ src/main/java/com/wanmoxing/jishu/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 	
-	@RequestMapping(value={"/", "/college", "/collegeDetail/*", "/StudentDetail", "/PersonalCenter", "/login", "/register", "/forgetPwd"})
+	@RequestMapping(value={"/", "/college", "/collegeDetail/*", "/StudentDetail/*", "/PersonalCenter", "/login", "/register", "/forgetPwd"})
 	public String home() {
 		return "index.html";
 	}

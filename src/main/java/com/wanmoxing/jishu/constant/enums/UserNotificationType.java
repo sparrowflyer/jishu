e3fe3b1ad44a57c755c2e62de47444cf4eb9892e
guxiren <872003327@qq.com>
2019-05-26 22:27:32 +0800

    关注通知 和 订单通知

--- src/main/java/com/wanmoxing/jishu/constant/enums/UserNotificationType.java
+++ src/main/java/com/wanmoxing/jishu/constant/enums/UserNotificationType.java
@@ -2,12 +2,12 @@ package com.wanmoxing.jishu.constant.enums;
 
 public enum UserNotificationType {
 	
-	NEW_FAN("关注",1), 
+	NEW_FAN("关注通知",1), 
 	NEW_PURCHASE("购买与付费",2),
 	ARTICLE_REPLY("评论",3), 
 	Good_LIKE("点赞和喜欢",4),
 	REPORT_ARTICLE("违规举报",5),
-	NEW_PURCHASE_CONTACT("购买联系方式",6);
+	NEW_PURCHASE_CONTACT("订单通知",6);
 	
 	private String type;
 	private int typeId;
